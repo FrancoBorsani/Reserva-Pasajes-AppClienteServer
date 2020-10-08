9be3e0b4ed230388ba3136397ff7633934fc4956
Ignacio Cazcarra <nacho.eze.cazcarra@gmail.com>
2020-10-07 22:21:16 -0300

    Hecho el menu basico de cliente.

--- Tp-Redes-Server/main.cpp
+++ Tp-Redes-Server/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <winsock2.h>
 #include <string>
+#include <conio.h>
 
 using namespace std;
 
@@ -52,24 +53,23 @@ public:
 };
 
 
+/***********************************************************************/
 std::string imprimirPasillo(){
     return "  | =======================================";
 }
+/***********************************************************************/
 
+/***********************************************************************/
 void imprimirAutobus(char asientos[3][20]){
-    cout<<"  |";
-    for(int i = 0; i < 29; i++){
-        if(i<18){
-            cout<<" ";
-        }
-        else if(i<28){
+    cout<<"  |                  ";
+    for(int i = 0; i < 11; i++){
+        if(i<10){
             cout<<" 1";
         }
         else{
             cout<<" 2"<<endl;
         }
     }
-    //std::string comienzo = "  |";
     cout<<"  |";
     int numeroAsiento = 1;
 
@@ -96,20 +96,105 @@ void imprimirAutobus(char asientos[3][20]){
         cout<<""<<endl;
     }
 
+    cout<<""<<endl;
 }
+/***********************************************************************/
+
+
+/***********************************************************************/
+//En realidad, el que acepta la conexion es el Servidor. La ultima parte de la funcion deberia ser en esa clase. Esto no deberia tener argumentos y se deberian enviar los input alla.
+bool verificarIpYPuerto(std::string ipReal, int puertoReal){
+
+    std::string ipInput="";
+    int puertoInput = 0;
+
+    while(ipInput==""){
+        cout<<"Ingrese la direccion IP"<<endl;
+        cin>>ipInput;
+        system("CLS");
+    }
+    while(puertoInput==0){
+        cout<<"Ingrese el puerto"<<endl;
+        cin>>puertoInput;
+        system("CLS");
+    }
+
+    int ipCmp = ipInput.compare(ipReal);
+    int puertoCmp = (puertoInput==puertoReal);
+
+    return (ipCmp==0 && puertoCmp==1)?true:false;
+}
+/***********************************************************************/
+
+
+/***********************************************************************/
+void menuCliente(){
+
+    char matriz[3][20] = {{'O','O','O','O','O','O','O','O','O','O','O','O','O','O','O','O','O','O','O','O'},
+    {'O','O','O','O','O','O','O','O','O','O','O','O','O','O','O','O','O','O','O','O'},
+    {'O','O','O','O','O','O','O','O','O','O','O','O','O','O','O','O','O','O','O','O'}};
+
+    bool respuesta = verificarIpYPuerto("192.178.0.0",4747);
+
+    while(!respuesta){
+       cout<<"Ip o puerto incorrecto. Vuelva a intentarlo."<<endl;
+       _getch();
+       system("CLS");
+       respuesta = verificarIpYPuerto("192.178.0.0",4747);
+    }
+
+
+    int servicioElegido = 0;
+
+    while(servicioElegido!=4){
+
+        cout<<"1-Alta servicio"<<endl;
+        cout<<"2-Gestionar pasajes"<<endl;
+        cout<<"3-Ver registro de actividades"<<endl;
+        cout<<"4-Cerrar sesion"<<endl;
+        cin>>servicioElegido;
+
+        switch(servicioElegido){
+            case 1: system("CLS");
+                    cout<<servicioElegido<<endl;
+                    _getch();
+                    system("CLS");
+                    break;
+            case 2: system("CLS");
+                    imprimirAutobus(matriz);
+                    _getch();
+                    system("CLS");
+                    break;
+            case 3: system("CLS");
+                    cout<<servicioElegido<<endl;
+                    _getch();
+                    system("CLS");
+                    break;
+            case 4: system("CLS");
+                    cout<<"Sesion cerrada correctamente"<<endl;
+                    _getch();
+                    system("CLS");
+                    break;
+            default: system("CLS");
+                    cout<<"Ingreso una opcion incorrecta."<<
+                    endl;
+                    _getch();
+                    system("CLS");
+                    break;
+        }
+    }
+
+}
+/***********************************************************************/
 
 
 int main()
 {
-    char matriz[3][20] = {{'O','O','O','O','O','O','O','O','O','O','O','O','O','O','O','O','O','O','O','O'},
-    {'O','O','O','O','O','O','O','O','O','O','O','O','O','O','O','O','O','O','O','O'},
-    {'O','O','O','O','O','O','O','O','O','O','O','O','O','O','O','O','O','O','O','O'}};
-    imprimirAutobus(matriz);
-
-  Server *Servidor = new Server();
-  while(true)
-  {
-     Servidor->Recibir();
-     Servidor->Enviar();
-  }
+    menuCliente();
+    Server *Servidor = new Server();
+    while(true)
+    {
+        Servidor->Recibir();
+        Servidor->Enviar();
+    }
 }
