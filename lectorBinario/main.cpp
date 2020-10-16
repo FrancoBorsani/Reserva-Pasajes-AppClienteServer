94da3924b32bfcc0092a9c7b682fd1b40ec76a43
FrancoBorsani <borsafranco@gmail.com>
2020-10-16 18:11:23 -0300

    Escritura y lectura de archivo binario

---
+++ lectorBinario/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <fstream>
+#include <string.h>
+using namespace std;
+
+int main()
+{
+    //VARIABLE A GUARDAR
+    char buffer2 [] = "Hola";
+
+    // ESCRITURA EN BINARIO
+   ofstream myFile ("escribirBin.bin", std::ios::ate | std::ios::in | ios :: binary);
+    myFile << buffer2;
+
+    myFile.close();
+
+    //LEER DEL ARCHIVO BINARIO
+    string mensaje;
+    ifstream leerArchivo ("escribirBin.bin", ios::in | ios::out | ios::binary);
+
+     while (!leerArchivo.eof()) {
+            leerArchivo >> mensaje;
+            cout << mensaje << endl;
+  }
+    //OTRA FORMA DE LECTURA
+  //  leerArchivo.seekg (0); //POSICIÓN DESDE LA QUE EMPIEZA A LEER
+  //  leerArchivo.read (mensaje, 4000);
+
+    leerArchivo.close();
+
+
+    return 0;
+}
